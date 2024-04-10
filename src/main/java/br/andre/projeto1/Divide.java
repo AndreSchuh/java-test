91c9b2726b0047909c31a85eb2872f67554fe2ae
Schuh <andregabschuh@gmail.com>
2024-04-10 15:00:26 -0300

    Criei as classes Multiplica e Divide. Usei algumas maneiras diferentes da soma.

--- src/main/java/br/andre/projeto1/Divide.java
+++ src/main/java/br/andre/projeto1/Divide.java
@@ -1,14 +1,13 @@
 package br.andre.projeto1;
 import java.util.Scanner;
 
-public class Multiplica {
+public class Divide {
 
     static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        float numero2, numero1, resultado;
 
-    public static void main(String[] args) {
-        int numero2, numero1;
-
-        System.out.println("\nMultiplicando dois números inteiros!\n");
+        System.out.println("\nDividindo dois números!\n");
 
         System.out.print("Digite o primeiro número inteiro: ");
         numero1 = sc.nextInt();
@@ -16,7 +15,7 @@ public class Multiplica {
         System.out.print("Digite o segundo número inteiro: ");
         numero2 = sc.nextInt();
 
-        System.out.printf("Resultado da multplicacao = %s", (numero1 * numero2));
-
+        resultado = numero1 / numero2;
+        System.out.printf("Resultado da divisao = %.2f", resultado);
     }
 }
