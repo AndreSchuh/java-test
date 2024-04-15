d2c60f0c2bcd31645c0cf4e3113b129db431f31c
Schuh <andregabschuh@gmail.com>
2024-04-15 09:50:28 -0300

    Finalizando a calculadora!

--- src/main/java/br/andre/projeto1/Calculadora.java
+++ src/main/java/br/andre/projeto1/Calculadora.java
@@ -1,61 +1,53 @@
-package br.andre.projeto1;
-import java.util.Scanner;
+    package br.andre.projeto1;
+    import java.util.Scanner;
 
-public class Calculadora {
-    static Scanner sc = new Scanner(System.in);
+    public class Calculadora {
+        static Scanner sc = new Scanner(System.in);
 
-    public static double numero1, numero2, resultado;
+        public static void main(String[] args) {
+            int operacao;
 
-    public static void main(String[] args) {
-        int operacao;
+            System.out.println("\nCalculadora!\n");
 
-        System.out.println("\nCalculadora!\n");
+           while (true){
+                System.out.println("Digite o primeiro número:");
+                double numero1 = sc.nextDouble();
 
-        do {
-            System.out.println("Digite o primeiro número:");
-            numero1 = sc.nextDouble();
+                System.out.println("Digite o segundo número:");
+                double numero2 = sc.nextDouble();
 
-            System.out.println("Digite o segundo número:");
-            numero2 = sc.nextDouble();
+                System.out.println("Escolha a operação: ");
+                System.out.println("1 - Soma");
+                System.out.println("2 - Subtração");
+                System.out.println("3 - Multiplicação");
+                System.out.println("4 - Divisão");
+                System.out.println("99 - Sair");
 
-            System.out.println("Escolha a operação: ");
-            System.out.println("1 - Soma");
-            System.out.println("2 - Subtração");
-            System.out.println("3 - Multiplicação");
-            System.out.println("4 - Divisão");
-            System.out.println("99 - Sair");
-            operacao = sc.nextInt();
-
-            if(1 == operacao){
-               resultado = soma();
-               System.out.println(""+ resultado);
-            }else if(2 == operacao){
-                resultado = subtracao();
-                System.out.println(""+ resultado);
-            }else if(3 == operacao){
-                resultado = multiplicacao();
-                System.out.println(""+ resultado);
-            }else if(4 == operacao){
-                resultado = divisao();
-                System.out.println(""+ resultado);
+                System.out.println("Digite a opção desejada");
+                operacao = sc.nextInt();
+                
+                switch(operacao){
+                    case 1:
+                        System.out.printf("Resultado da soma = %s", soma(numero1, numero2));
+                        break;
+                    case 2:
+                        System.out.printf("Resultado da soma = %s", subtracao(numero1, numero2));
+                        break;
+                    case 3:
+                        System.out.printf("Resultado da soma = %s", divisao(numero1, numero2));
+                        break;
+                    case 4:
+                        System.out.printf("Resultado da soma = %s", multiplicacao(numero1, numero2));
+                        break;
+                    default:
+                    case 99:
+                        System.out.print("Saindo!");
+                        System.exit(0);
+                }
             }
-        } while (operacao >= 1 && operacao <= 4);
-    }
+        }
 
-    public static double soma() {
-        resultado = numero1 + numero2;
-        return resultado;
-    }
-    public static double subtracao() {
-        resultado = numero1 - numero2;
-        return resultado;
-    }
-    public static double multiplicacao() {
-        resultado = numero1 * numero2;
-        return resultado;
-    }
-    public static double divisao() {
-        resultado = numero1 / numero2;
-        return resultado;
-    }
-}
+        public static double soma(double a, double b){return a + b;}
+        public static double subtracao(double a, double b){return a - b;}
+        public static double divisao(double a, double b){return a / b;}
+        public static double multiplicacao(double a, double b){return a * b;}}
