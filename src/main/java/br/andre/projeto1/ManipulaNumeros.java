ea3886f80cda3387fe6a2a2624ff923ef3d0ffba
Elieser Prichua <elieser.prichua@hypera.com.br>
2024-04-15 09:50:44 -0300

    Criada classes para desafio.

---
+++ src/main/java/br/andre/projeto1/ManipulaNumeros.java
@@ -0,0 +1,41 @@
+package br.andre.projeto1;
+
+import java.util.Arrays;
+
+public class ManipulaNumeros {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40};
+
+    }
+
+    public static void imprimeNumerosPares(int[] numeros) {
+        // TODO implementar
+    }
+
+    public static void imprimeNumerosPrimos(int[] numeros) {
+        // TODO implementar
+    }
+
+    public static void calculaBaskara(int a, int b, int c) {
+        // TODO implementar, recebe quaisquer 3 números fixos
+    }
+
+    public static void imprimeAleatorios(int[] numeros, int quantidade) {
+        // TODO implementar, onde quantidade deve ser quantos números aleatórios do array numeros devem ser impressos
+    }
+
+    public static void calculaMedia(int[] numeros, int quantidade) {
+        // TODO implementar
+        // quantidade de números aleatórios do array "numeros"
+        // deve imprimir os números aleatórios e em seguida a média deles
+    }
+
+    public static void calculaMediaGeometrica(int[] numeros, int quantidade) {
+        // TODO implementar
+        // quantidade de números aleatórios do array "numeros"
+        // deve imprimir os números aleatórios e em seguida a média geométrica deles
+    }
+}
