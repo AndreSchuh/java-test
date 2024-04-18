d2629767c337acc4f067d871e366127f047846c9
Elieser Prichua <elieser.prichua@hypera.com.br>
2024-04-18 11:23:19 -0300

    Adicionado comentários de ajuda.

--- src/main/java/br/andre/projeto1/ManipulaNumeros.java
+++ src/main/java/br/andre/projeto1/ManipulaNumeros.java
@@ -11,38 +11,65 @@ public class ManipulaNumeros {
         int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
                 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40};
 
+        imprimeNumerosPares(numeros);
+//        imprimeNumerosPrimos(numeros);
+//        calculaBaskara(1,12,-13);
+//        imprimeAleatorios(numeros, 7);
+//        calculaMedia(numeros, 5);
+
     }
 
     public static void imprimeNumerosPares(int[] numeros) {
-        // TODO implementar
-        System.out.println("\nNúmero par ou ímpar!\n");
+
+        //Só faltou imprimir o número que é par, e era pra imprimir só os pares mesmo, conforme o nome do método
+        System.out.println("\nNúmeros pares!\n");
         for ( int numero : numeros){ // Essa parte de Enhanced for loop eu tive que pesquisar
             if(numero % 2 == 0){
-                System.out.println("Número par");
-            }else{
-                System.out.println("Número ímpar");
+                System.out.println("Número par: "+ numero);
             }
         }
+
+        //Pega a sacada: impressão de números pares usando Stream (Stream é uma forma de manipular coleções de dados)
+        //Veja como é possível fazer em uma única linha....
+        //Arrays.stream(numeros).filter(n -> n % 2 == 0).forEach(System.out::println);
     }
 
     public static void imprimeNumerosPrimos(int[] numeros) {
         // TODO implementar
         // Dei uma pesquisada vou precisar de mais tempo pra resolver esse
+
+        // É uma lógica simples, primo é aquele que só divide por um e por ele mesmo, ou seja
+        // se numero % 1 == 0 e numero % numero == 0 E qualquer numero entre 1 e ele mesmo não der resto 0
+        // cai muito em prova de programação
+
     }
 
     public static void calculaBaskara(int a, int b, int c) {
-        // TODO implementar, recebe quaisquer 3 números fixos
-        a = 3; // Era pra declarar os números assim?
-        b = 6;
-        c = 25;
+
+        // Era pra declarar os números assim?
+        //Não exatamente aqui dentro, pois vc tem as três variáveis como parâmetros na assinatua do método, então lá
+        //dentro do método "main" vc declara as variáveis e chama o calculaBaskara(3, 6, 25). Deixei comentado lá em cima, só tirar o comentário
+//        a = 3;
+//        b = 6;
+//        c = 25;
+
+        // o "+-" da forma que vc colocou sempre vai ser menos (mais com menos = menos)
+        //Dica, quebrar em partes:
+        //Sugiro primeiro calcular o Delta e associar a uma variável
+        //A operação de segundo grau espera dois números, então a dica é realizar operação de raiz quadrada separadamente,
+        //e na hora de imprimir pensa em algo do tipo: " As raizes são x1 = "val" e x2 = "val" "
         double resultado = -b +- Math.sqrt((b*b) - 4 * a * c)/ 2 * a;
-        System.out.printf("Resultado = %.2f" + resultado);
+        System.out.printf("Resultado %.2f", resultado);
     }
 
     public static void imprimeAleatorios(int[] numeros, int quantidade) {
         // TODO implementar, onde quantidade deve ser quantos números aleatórios do array numeros devem ser impressos
         System.out.println("\nAleatório!\n");
 
+        //a quantidade não pode receber um valor aqui dentro do método, ela é um parâmetro do método
+        //e representa a quantidade de números aleatórios que vc quer imprimir
+        //Dica: tu vai precisar de um for de 0 até a quantidade (recebido no parâmetro), e dentro do for vc vai
+        //mandar imprimir: gerador.nextInt(numeros.length). Isso vai gerar um número aleatório entre 0 e o tamanho do array
         quantidade = gerador.nextInt(numeros.length);
 
         int numeroAleatorio = numeros[quantidade];
@@ -50,7 +77,14 @@ public class ManipulaNumeros {
         }
 
     public static void calculaMedia(int[] numeros, int quantidade) {
-        // TODO implementar
+        /*
+        Explicando melhor:
+        Você tem um array de 40 números lá no "main" e esse método recebe o array e a quantidade de números que você quer gerar média.
+        O método deve escolher aleatoriamente a quantidade de números que você passou no parâmetro e gerar a média deles.
+        Deve ser impresso os números aleatórios e a média deles.
+        É parecido com o de cima, conforme vc escolher o numero aleatório, vc imprime ele e vai somando em uma variável
+        Depois de imprimir todos, fora do "for" vc divide a variável acumulada pela quantidade de números e apresenta a média.
+         */
         // quantidade de números aleatórios do array "numeros"
         // deve imprimir os números aleatórios e em seguida a média deles
         quantidade = gerador.nextInt(numeros.length);
@@ -59,12 +93,5 @@ public class ManipulaNumeros {
         System.out.printf("Número aleatório da array = " + numeroAleatorio);
 
         int a = (numeroAleatorio.length); // Tive a ideia de usar a length porém ...
-    }
-
-    public static void calculaMediaGeometrica(int[] numeros, int quantidade) {
-        // TODO implementar
-        // quantidade de números aleatórios do array "numeros"
-        // deve imprimir os números aleatórios e em seguida a média geométrica deles
-        // Esse daqui eu nem tentei, estava tentando resolver os outros primeiros
     }
 }
