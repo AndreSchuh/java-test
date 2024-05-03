9852007ba828142a309d25429df0e759284a42fb
Schuh <andregabschuh@gmail.com>
2024-05-03 11:17:15 -0300

    Fiz as implementações porém não consegui testar o código

--- src/main/java/br/andre/projeto1/ManipulaBigDecimal.java
+++ src/main/java/br/andre/projeto1/ManipulaBigDecimal.java
@@ -4,25 +4,28 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class ManipulaBigDecimal {
-    public static void main(String[] args) {
-        System.out.println("Manipulando BigDecimals");
-
-        BigDecimal a = BigDecimal.valueOf(15.0);
+    public static  BigDecimal.valueOf(15.0);
         BigDecimal b = BigDecimal.valueOf(7.0);
-        BigDecimal c = BigDecimal.valueOf(3.0);
+        BigDecimal c = Bigvoid main(String[] args) {
+            System.out.println("Manipulando BigDecimals");
+
+            BigDecimal a =Decimal.valueOf(3.0);
 
     }
 
     public static void soma(BigDecimal a, BigDecimal b, BigDecimal c){
         //TODO implementar
+        System.out.println(a.add(b).add(c));
     }
 
     public static void subtrair(BigDecimal a, BigDecimal b){
         //TODO implementar
+        System.out.println(a.subtract(b));
     }
 
     public static void multiplicar(BigDecimal a, BigDecimal b, BigDecimal c){
         //TODO implementar
+        System.out.println(a.multiply(b).multiply(c));
     }
 
     public static void dividir(BigDecimal b, BigDecimal c){
@@ -34,9 +37,13 @@ public class ManipulaBigDecimal {
         // RoundingMode.HALF_DOWN
         // RoundingMode.HALF_UP
         // ex.: b.divide(c, 2, RoundingMode.HALF_UP);
+        System.out.println(b.divide(c, 3, RoundingMode.UP));
     }
 
     public static void baskara(BigDecimal a, BigDecimal b, BigDecimal c){
         //TODO implementar
+        BigDecimal delta = b.multiply(b).subtract(BigDecimal.valueOf(4.00)).multiply(a).multiply(c);
+        System.out.println(BigDecimal.valueOf(0).subtract(b).add(delta).divide(BigDecimal.valueOf(2)).multiply(a));
+        System.out.println(BigDecimal.valueOf(0).subtract(b).subtract(delta).divide(BigDecimal.valueOf(2)).multiply(a));
     }
 }
