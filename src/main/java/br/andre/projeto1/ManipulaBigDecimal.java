c39770c1477386531b22dbea2a374c60095f96ef
Schuh <andregabschuh@gmail.com>
2024-05-03 11:37:01 -0300

    Fiz as implementações porém não consegui testar o código

--- src/main/java/br/andre/projeto1/ManipulaBigDecimal.java
+++ src/main/java/br/andre/projeto1/ManipulaBigDecimal.java
@@ -4,28 +4,27 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class ManipulaBigDecimal {
-    public static  BigDecimal.valueOf(15.0);
-        BigDecimal b = BigDecimal.valueOf(7.0);
-        BigDecimal c = Bigvoid main(String[] args) {
-            System.out.println("Manipulando BigDecimals");
+    public static void main(String[] args) {
+        System.out.println("Manipulando BigDecimals");
 
-            BigDecimal a =Decimal.valueOf(3.0);
+        BigDecimal a = BigDecimal.valueOf(15.0);
+        BigDecimal b = BigDecimal.valueOf(7.0);
+        BigDecimal c = BigDecimal.valueOf(3.0);
 
     }
-
     public static void soma(BigDecimal a, BigDecimal b, BigDecimal c){
         //TODO implementar
-        System.out.println(a.add(b).add(c));
+        System.out.printf("O resultado da soma é %s", a.add(b).add(c));
     }
 
     public static void subtrair(BigDecimal a, BigDecimal b){
         //TODO implementar
-        System.out.println(a.subtract(b));
+        System.out.printf("O resultado da subtração é %s", a.subtract(b));
     }
 
     public static void multiplicar(BigDecimal a, BigDecimal b, BigDecimal c){
         //TODO implementar
-        System.out.println(a.multiply(b).multiply(c));
+        System.out.printf("O resultado da multiplicação é %s", a.multiply(b).multiply(c));
     }
 
     public static void dividir(BigDecimal b, BigDecimal c){
@@ -37,13 +36,13 @@ public class ManipulaBigDecimal {
         // RoundingMode.HALF_DOWN
         // RoundingMode.HALF_UP
         // ex.: b.divide(c, 2, RoundingMode.HALF_UP);
-        System.out.println(b.divide(c, 3, RoundingMode.UP));
+        System.out.printf("O resultado da divisão é %s", b.divide(c, 3, RoundingMode.UP));
     }
 
     public static void baskara(BigDecimal a, BigDecimal b, BigDecimal c){
         //TODO implementar
         BigDecimal delta = b.multiply(b).subtract(BigDecimal.valueOf(4.00)).multiply(a).multiply(c);
-        System.out.println(BigDecimal.valueOf(0).subtract(b).add(delta).divide(BigDecimal.valueOf(2)).multiply(a));
-        System.out.println(BigDecimal.valueOf(0).subtract(b).subtract(delta).divide(BigDecimal.valueOf(2)).multiply(a));
+        System.out.printf("O resultado da primeira raíz é %s", BigDecimal.valueOf(0).subtract(b).add(delta).divide(BigDecimal.valueOf(2), 3,RoundingMode.UP).multiply(a));
+        System.out.printf("O resultado da segunda raíz é %s", BigDecimal.valueOf(0).subtract(b).subtract(delta).divide(BigDecimal.valueOf(2), 3,RoundingMode.UP).multiply(a));
     }
 }
