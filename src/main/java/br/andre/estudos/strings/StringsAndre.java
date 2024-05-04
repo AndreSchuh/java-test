16914f9dddfcab197386e383540aba080aa20426
Elieser Prichua <elieser.prichua@hypera.com.br>
2024-05-04 14:49:47 -0300

    Criado desafio 1 com strings. Incluído classe de apoio.

---
+++ src/main/java/br/andre/estudos/strings/StringsAndre.java
@@ -0,0 +1,10 @@
+package br.andre.estudos.strings;
+
+public class StringsAndre {
+    public static void main(String[] args) {
+
+        //TODO Criar um programa que solicite ao usuário que digite uma palavra e imprima a quantidade de caracteres da palavra.
+        // Criar um método (fora do main) que recebe uma palavra e retorna a quantidade de caracteres da palavra.
+        // A impressão deve ser feita no main.
+    }
+}
