fcebd1c37fceafd9ce0357f396e8089b6a0a783a
Schuh <andregabschuh@gmail.com>
2024-05-05 20:44:57 -0300

    Desafio resolvido!

--- src/main/java/br/andre/estudos/strings/StringsAndre.java
+++ src/main/java/br/andre/estudos/strings/StringsAndre.java
@@ -1,10 +1,31 @@
 package br.andre.estudos.strings;
+import java.util.Scanner;
 
 public class StringsAndre {
+    public static String palavra;
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String inicio = """
+        \nQuantidade de caractereres!\n
+        Digite uma palavra: """;
+
+        System.out.println(inicio);
+        palavra = sc.nextLine();
+
+        System.out.printf("A quantidade de caracteres é %d", quantidade());
+        //System.out.printf("A quantidade de caracteres é %d", palavra.length());
 
         //TODO Criar um programa que solicite ao usuário que digite uma palavra e imprima a quantidade de caracteres da palavra.
         // Criar um método (fora do main) que recebe uma palavra e retorna a quantidade de caracteres da palavra.
         // A impressão deve ser feita no main.
+
+
+    }
+
+    public static int quantidade(){
+        int quantidade = palavra.length();
+        return quantidade;
     }
 }
