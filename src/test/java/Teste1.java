7c1b310b582f3fc7f558e74f2ffe2e343526bee3
Elieser Prichua <elieser.prichua@hypera.com.br>
2024-05-05 22:31:13 -0300

    Enviando Teste1.java

---
+++ src/test/java/Teste1.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Teste1 {
+    @Test
+    public void test1() {
+        Assertions.assertTrue(true);
+    }
+}
