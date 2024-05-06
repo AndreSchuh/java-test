dd4c52b047c37874ec87c8dec084943f59cf5cdb
Schuh <andregabschuh@gmail.com>
2024-05-06 20:58:04 -0300

    Finalizando issue

--- src/test/java/br/andre/estudos/strings/StringProcessamentoTest.java
+++ src/test/java/br/andre/estudos/strings/StringProcessamentoTest.java
@@ -5,20 +5,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringsAndreTest {
+class StringProcessamentoTest {
+    StringProcessamento principal = new StringProcessamento();
 
     @Test
     @DisplayName("Testa o método que retorna a quantidade de caracteres de uma palavra")
-    void quantidadeTest() {
-        StringsAndre.palavra = "Andre";
-        int resultado = StringsAndre.quantidade();
+    void quantidadeCaracteresTest() {
+        String texto = "Andre";
+        int resultado = principal.quantidadeCaracteres(texto);
         assertEquals(5, resultado); //deve ser 5
         assertNotEquals(6, resultado); //não deve ser 6
     }
+
     @Test
-    void inversaoTest(){
-        StringsAndre.palavra = "Elieser Prichua";
-        String resultado = StringsAndre.inverso();
+    @DisplayName("Testa se a palavra foi invertida de forma correta")
+    void inverteTextoTest(){
+        String texto = "Elieser Prichua";
+        String resultado = principal.inverteTexto(texto);
         assertEquals("auhcirP reseilE", resultado);
         assertNotEquals("Elieser Prichua", resultado);
         assertNotEquals("reseile auhcirp", resultado);
