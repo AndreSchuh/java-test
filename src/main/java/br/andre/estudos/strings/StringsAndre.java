129d4756c69cd6751137908f68633bd6530d363e
Schuh <andregabschuh@gmail.com>
2024-05-06 11:17:42 -0300

    Adicionado o menu

--- src/main/java/br/andre/estudos/strings/StringsAndre.java
+++ src/main/java/br/andre/estudos/strings/StringsAndre.java
@@ -2,30 +2,45 @@ package br.andre.estudos.strings;
 import java.util.Scanner;
 
 public class StringsAndre {
-    public static String palavra;
-    public static void main(String[] args) {
+    public  static String palavra;
 
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String inicio = """
-        \nQuantidade de caractereres!\n
-        Digite uma palavra: """;
 
-        System.out.println(inicio);
-        palavra = sc.nextLine();
+        while(true) {
 
-        System.out.printf("A quantidade de caracteres é %d", quantidade());
-        //System.out.printf("A quantidade de caracteres é %d", palavra.length());
+            String menu = """
+                     \nQuantidade de caractereres!\n
+                    1 - Contar letras do texto
+                    99 - Sair 
+                    Digite a opção desejada\n""";
+            System.out.print(menu);
+            int resposta = sc.nextInt();
+            sc.nextLine();
 
+            switch(resposta){
+                case 1:
+            System.out.println("Digite uma palavra:");
+            palavra = sc.nextLine();
+            System.out.println("A quantidade de caracteres é "+ quantidade(palavra));
+            break;
+
+                case 99:
+                    System.out.println("Encerrando");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
         //TODO Criar um programa que solicite ao usuário que digite uma palavra e imprima a quantidade de caracteres da palavra.
         // Criar um método (fora do main) que recebe uma palavra e retorna a quantidade de caracteres da palavra.
         // A impressão deve ser feita no main.
 
-
     }
 
-    public static int quantidade(){
-        int quantidade = palavra.length();
-        return quantidade;
+    public static int quantidade(String letra){
+        return letra.length();
     }
 }
