30220d4cc5101a676344e719c10d7eeaabb8ab9c
andre <andregabschuh@gmail.com>
2024-06-08 10:03:35 -0300

    Desenvolvimento de treeMap e LinkedHashMap

--- src/main/java/br/andre/estudos/mapas/BaseMaps.java
+++ src/main/java/br/andre/estudos/mapas/BaseMaps.java
@@ -1,6 +1,7 @@
 package br.andre.estudos.mapas;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -78,13 +79,53 @@ public class BaseMaps {
 
 
 //        Faça exemplos que usem TreeMap, LinkedHashMap e explique a diferença entre eles sobre ordenação e valores nulos.
-//        TODO implementa exemplos abaixo
 
+//        O treMap possui uma ordenação por padrão aleatória coisa que não acontece no hashMap que mantém sua
+//        ordenação por inserção e também o linkedHashMap permite chaves nulas o treMap já não como representado no código.
 
+        TreeMap<Integer, String> numeros = new TreeMap<>(); // Cria um treeMap
+
+        System.out.println();
+        System.out.println();
+        System.out.println("TREE MAP!");
+
+        numeros.put(3, "Três"); //Adiciona elementos ao map
+        numeros.put(2, "Dois");
+        numeros.put(1,"Um");
+//      numeros.put(null, "Zero"); CHAVE NULA
+        System.out.println("-----------------------------------");
+        System.out.println(numeros);// Exibe o map inteiro
+        System.out.println("-----------------------------------");
+
+        System.out.println(numeros.get(2));// Exibe chave específica
+        System.out.println("-----------------------------------");
+
+        numeros.remove(2); // Remove chave específica
+        System.out.println(numeros);
+        numeros.get(2); // Recupera chave removida
+        System.out.println(numeros);
+
+        System.out.println(numeros.keySet()); // Exibe as chaves em ordem crescente fato que já acontece automaticamente no LinkedHashMap
+
+        System.out.println();
+        System.out.println();
+        System.out.println("LINKED HASH MAP!");
+
+        LinkedHashMap<String, String> capitais = new LinkedHashMap<>();
+
+        capitais.put("Salvador", "Bahia");
+        capitais.put("Belém", "Pará");
+        capitais.put("Boa vista", "Roraima");
+        capitais.put(null, "Porto Alegre"); //Chave nula
+
+        System.out.println(capitais); //Mostra o mapa e como pode se observar está na ordem de inserção
 
 
 //        Agora explique com suas palavras quando é melhor usar um ou outro (pode ser mediante comentários).
 
+//        Se no projeto onde está sendo utilizado maps não é necessário pouca memória, inserções nulas e uma velocidade
+//        de inserção e busca boa o treeMap é recomendado para isso. Já o linkedHashMap utiliza uma memória menor e possibilidade
+//        de chaves nulas. Porém sua performance acaba sendo mais lenta comparado com o outro.
 
     }
 }
